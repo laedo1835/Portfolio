a2fc082624cfb89982867740ad7599ba79268732
laedo1835 <nickmcfee@gmail.com>
2024-03-17 09:06:19 -0500

    fixed navbar error

--- main.js
+++ main.js
@@ -18,14 +18,14 @@ function onClick(element) {
       }
   }
   
-  /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-  function myFunction() {
-      var x = document.getElementById("myTopnav");
-      if (x.className === "topnav") {
+    /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+    function menuFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
         x.className += " responsive";
-      } else {
+        } else {
         x.className = "topnav";
-      }
+        }
     }
   
   
